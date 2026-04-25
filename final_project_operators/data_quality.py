143cc2b9c331fd45ebd510587cb6cf5f19dffda3
José Andrés sedán Espitia <109873223+jasedane75@users.noreply.github.com>
2026-04-25 08:29:29 -0500

    Update data_quality.py

--- final_project_operators/data_quality.py
+++ final_project_operators/data_quality.py
@@ -27,17 +27,22 @@ class DataQualityOperator(BaseOperator):
             
             self.log.info(f"Ejecutando prueba: {test['check_sql']}")
             result = redshift.get_first(test['check_sql'])
-
+            
+            """Validaciones relacionadas a la integridad y calidad de los datos"""
+            
             if result is None or len(result) == 0:
+                self.log.info(f"Validacion de resultados fallida en: {test['check_sql']}")
                 raise ValueError(f"Prueba no devolvió resultados: {test['check_sql']}")
-
+                
             if 'expected_value' in test and result[0] != test['expected_value']:
+                self.log.info(f"Validacion de resultados fallida en: {test['check_sql']}")
                 raise ValueError(
                     f"Prueba falló: {test['check_sql']}. "
                     f"Esperado {test['expected_value']}, obtenido {result[0]}"
                 )
 
             if 'expected_min' in test and result[0] < test['expected_min']:
+                self.log.info(f"Validacion de resultados fallida en: {test['check_sql']}")
                 raise ValueError(
                     f"Prueba falló: {test['check_sql']}. "
                     f"Mínimo esperado {test['expected_min']}, obtenido {result[0]}"
